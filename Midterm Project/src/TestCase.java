192de160b446354f98fa2c01750225ba508a5929
pooya <pooya@LAPTOP-8LPAM0CU.lan>
2022-03-26 13:32:16 -0700

    Commit

---
+++ Midterm Project/src/TestCase.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestCase {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+
+		@Test
+		public void helloTest() {
+			System.out.println("Hello World!");
+		}
+	
+
+}
