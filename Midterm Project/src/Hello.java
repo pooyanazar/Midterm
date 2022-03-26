a832ec6a00501d5928e68cb45aa8ca6612031609
pooyanazar <101955807+pooyanazar@users.noreply.github.com>
2022-03-26 13:37:43 -0700

    Update Hello.java

--- Midterm Project/src/Hello.java
+++ Midterm Project/src/Hello.java
@@ -1,5 +1,9 @@
 class Hello{
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        //This is Test
+       /*
+       This is test
+       */
     }
 }
