3fc6b955ce074ea6ebc6ca6b3da9df65e373e319
pooyanazar <101955807+pooyanazar@users.noreply.github.com>
2022-04-04 00:06:01 -0700

    Update TestCase.java

--- Midterm Project/src/TestCase.java
+++ Midterm Project/src/TestCase.java
@@ -32,7 +32,7 @@ class TestCase {
 
 		@Test
 		public void helloTest() {
-			System.out.println("Hello World!Java OnlyItFor040320222357");
+			System.out.println("Hello World!Java OnlyItFor040420220005");
 		}
 	
 
